98d1b359762f8a54d9487d564663e72e7f155bb8
techgal14 <189483818+techgal14@users.noreply.github.com>
2026-04-13 13:41:24 -0700

    add typewriter text to contact hero and projects hero

---
+++ src/interact.js
@@ -0,0 +1,21 @@
+var projectsHero = document.getElementById("projects-hero");
+
+var typewriter = new Typewriter(projectsHero, {
+  autoStart: true,
+  cursor: "",
+  delay: 75,
+  strings: "Check out some of the projects that I've been working on below!",
+});
+
+var contactHero = document.getElementById("contact-hero");
+
+var typewriter = new Typewriter(contactHero, {
+  autoStart: true,
+  cursor: "",
+  delay: 75,
+  strings: "Simple or complex - let's bring your vision to life!",
+});
+
+
+    
+
